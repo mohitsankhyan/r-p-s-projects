20e8a3911a354054810b4f74474d052443ca209c
mohitsankhyan <mohitsankhyan81@gmail.com>
2024-09-19 15:12:28 +0530

    Update r-p-s-projects.c

--- r-p-s-projects.c
+++ r-p-s-projects.c
@@ -5,40 +5,30 @@
 #include <stdlib.h>
 #include <time.h>
  
-// Function to implement the game
+
 int game(char you, char computer)
 {
-    // If both the user and computer
-    // has choose the same thing
     if (you == computer)
         return -1;
  
-    // If user's choice is stone and
-    // computer's choice is paper
     if (you == 's' && computer == 'p')
         return 0;
  
-            // If user's choice is paper and
-            // computer's choice is stone
             else if (you == 'p' && computer == 's') return 1;
  
-    // If user's choice is stone and
-    // computer's choice is scissor
+
     if (you == 's' && computer == 'z')
         return 1;
  
-    // If user's choice is scissor and
-    // computer's choice is stone
+    
     else if (you == 'z' && computer == 's')
         return 0;
  
-    // If user's choice is paper and
-    // computer's choice is scissor
+    
     if (you == 'p' && computer == 'z')
         return 0;
  
-    // If user's choice is scissor and
-    // computer's choice is paper
+    
     else if (you == 'z' && computer == 'p')
         return 1;
 }
